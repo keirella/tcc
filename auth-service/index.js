81503ffdd8bc8c5650af602d09745b784bd24c65
Riella <atiqazahrani@gmail.com>
2026-05-25 10:43:40 +0700

    update

--- auth-service/index.js
+++ auth-service/index.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 
 // Gunakan routes yang akan di buat
 app.use('/auth', authRoutes);
-app.use(cors());
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
